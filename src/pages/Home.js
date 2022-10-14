261614cb30724c6b65c30bf6a0a332c8747c834c
mhanifmustakim <mhanifmustakim@gmail.com>
2022-10-14 14:58:17 +0800

    Add content and design for home Section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,23 @@
+import HomePageImage from "/src/Images/gabrielle-henderson-FqB_Kj7PZa8-unsplash.jpg";
+
 export default function () {
-    const content = document.createElement("h1");
-    content.textContent = "Welcome to Home Page";
-    return content
+    const home = document.createElement("div");
+    home.id = "home";
+
+    const mainText = document.createElement("h1");
+    mainText.innerHTML = "Welcome to Sugar Rush Cafe";
+    mainText.classList.add("text-cursive");
+    mainText.classList.add("text-center");
+
+    const p = document.createElement("p");
+    p.classList.add("text-center");
+    p.innerText = "Where sweet people to get sweeter."
+
+    const img = document.createElement("img");
+    img.src = HomePageImage;
+
+    home.appendChild(img);
+    home.appendChild(mainText);
+    home.appendChild(p);
+    return home
 }
